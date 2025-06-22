437701de8a5ef0ccfc99bcbae84d80647446e92a
CaroLaura <caroline.buquet8@orange.fr>
2025-06-22 08:55:32 +0200

    Add files via upload

---
+++ annuaire.js
@@ -0,0 +1,58 @@
+const annuaire = [
+  {
+    cle_primaire: "1",
+    nom: "Ferme du Chassy",
+    type1: "boutique terroir",
+    type2: "fruits et légumes",
+    photo1: "",
+    int: "1",
+    adresse: "rue bidule",
+    commune: "Esquennoy",
+    code_postal: "60120",
+    description: "vendeur d'esquennoy fruits et légumes, spécial fraises",
+    long: "2",
+    lat: "49"
+  },
+  {
+    cle_primaire: "1",
+    nom: "Yaourt Fléchy",
+    type1: "boutique terroir",
+    type2: "produits laitiers",
+    photo1: "",
+    int: "1",
+    adresse: "rue bidule",
+    commune: "Fléchy",
+    code_postal: "60120",
+    description: "très bon",
+    long: "2",
+    lat: "49"
+  },
+  {
+    cle_primaire: "1",
+    nom: "Boutique hérépres",
+    type1: "boutique terroir",
+    type2: "viandes",
+    photo1: "",
+    int: "1",
+    adresse: "rue bidule",
+    commune: "Essertaux",
+    code_postal: "80",
+    description: "vendeur boutique familiale",
+    long: "2",
+    lat: "49"
+  },
+  {
+    cle_primaire: "1",
+    nom: "Ferme Bonneuil",
+    type1: "boutique terroir",
+    type2: "fruits et légumes",
+    photo1: "",
+    int: "1",
+    adresse: "rue bidule",
+    commune: "Bonneuil-les-eaux",
+    code_postal: "60120",
+    description: "vente lait frais",
+    long: "2",
+    lat: "49"
+  }
+];
---
+++ evenements.js
@@ -0,0 +1,74 @@
+const evenements = [
+  {
+    date: "2025-06-22",
+    titre: "Fanfare Fête de la musique",
+    période: "ponctuel",
+    description: "Prestation des élèves musiciens et Accordéonissimo",
+    horaire: "",
+    lieu: "Place du Beau-bois 60120 Bonneuil les eaux"
+  },
+  {
+    date: "2025-06-28",
+    titre: "Nuit des arts",
+    période: "ponctuel",
+    description: "Concerts, film, jeux, etc",
+    horaire: "14h -18h et 18h-00h30",
+    lieu: "Musée Archéologique de l'Oise Vendeuil-Caply"
+  },
+  {
+    date: "2025-08-22",
+    titre: "Souffle de la Terre",
+    période: "estival",
+    description: "Du 22/08 au 20/09. Spectacle d'1h30 avec 3 500 personnages",
+    horaire: "",
+    lieu: "80250 Ailly-sur-Noye"
+  },
+  {
+    date: "2025-07-13",
+    titre: "Fête des fruits rouges",
+    période: "ponctuel",
+    description: "fête locale",
+    horaire: "",
+    lieu: "60 Noyon"
+  },
+  {
+    date: "2025-06-20",
+    titre: "Fête de la musique",
+    période: "ponctuel",
+    description: "Concert gratuit Orchestre d'harmonie de Breteuil.",
+    horaire: "19h - 23h",
+    lieu: "60120 Breteuil"
+  },
+  {
+    date: "2025-07-13",
+    titre: "Fête nationale - Concert et Feu d'artifice",
+    période: "ponctuel",
+    description: "gratuit",
+    horaire: "20h30",
+    lieu: "60120 Breteuil"
+  },
+  {
+    date: "2025-08-23",
+    titre: "Médiévales de Folleville",
+    période: "ponctuel",
+    description: "Week-end médiévale. Restauration médiévale (Jambon à la broche).",
+    horaire: "10-19h",
+    lieu: "80250 Folleville"
+  }, 
+  {
+    date: "2025-08-01",
+    titre: "Nuit des étoiles",
+    période: "ponctuel",
+    description: "Association Repères Astro. Initiation à la manipulation et au réglages d’instruments d’observation. Exposition d’astrophotographies faites par les membres de l’Association Repères Astro. Conférences sur la mythologie.",
+    horaire: "16h-00h",
+    lieu: "60120 Lavacquerie"
+  }, 
+  {
+    date: "2025-06-21",
+    titre: "Journée de l'Astronomie Solaire de l'Oise et du Nord",
+    période: "ponctuel",
+    description: "Par les associations MARS60 et REPERES. Sur inscription (100 places).",
+    horaire: "18h",
+    lieu: "60300 Baron"
+  }
+];
